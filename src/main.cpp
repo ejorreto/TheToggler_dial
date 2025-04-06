517ff9f8c38ee264a6453a1c0c70471381d860e0
Enrique Jorreto <ejorreto@gmail.com>
2025-04-06 08:23:32 +0200

    [TTOG-1] Updated main to handle the wifi connection in its own function. Updated pointer to Toggl API

--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,58 @@
 #include "credentials.h"
 
 Task favouriteTasks[] = {
-    Task(1, "Task 1"),
-    Task(2, "Task 2"),
-    Task(3, "Task 3"),
-    Task(4, "Task 4"),
-    Task(5, "Task 5")};
+    Task(0, "Stop tracking", 0),
+    Task(1, "Reu I", projectOneId),
+    Task(2, "Reu D", projectTwoId),
+    Task(3, "General", projectTwoId),
+    Task(4, "Task 4", projectTwoId),
+    Task(5, "Task 5", projectTwoId)};
 
 int numOfTasks = sizeof(favouriteTasks) / sizeof(favouriteTasks[0]);
 
 Toggl toggl;
+
+bool wifiConnect()
+{
+  int numRetries = 5;
+  M5Dial.Display.clear();
+  M5Dial.Display.drawString("Connecting",
+                            M5Dial.Display.width() / 2,
+                            M5Dial.Display.height() / 2);
+  while (WiFi.status() != WL_CONNECTED && numRetries > 0)
+  {
+    WiFi.begin(mainWifiSSID, mainWifiPass);
+    delay(1000);
+    numRetries--;
+  }
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    numRetries = 5;
+    while (WiFi.status() != WL_CONNECTED && numRetries > 0)
+    {
+      WiFi.begin(fallbackWifiSSID, fallbackWifiPass);
+      delay(1000);
+      numRetries--;
+    }
+  }
+
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    M5Dial.Display.clear();
+    M5Dial.Display.drawString("Wifi ON",
+                              M5Dial.Display.width() / 2,
+                              M5Dial.Display.height() / 2);
+  }
+  else
+  {
+    M5Dial.Display.clear();
+    M5Dial.Display.drawString("Wifi OFF",
+                              M5Dial.Display.width() / 2,
+                              M5Dial.Display.height() / 2);
+  }
+  delay(1000);
+  return WiFi.status() == WL_CONNECTED;
+}
 
 String formatISODate(const String &dateTime)
 {
@@ -37,49 +80,11 @@ void setup()
   M5Dial.Display.setTextDatum(middle_center);
   M5Dial.Display.setTextFont(&fonts::Orbitron_Light_32);
   M5Dial.Display.setTextSize(0.75);
-  M5Dial.Display.clear();
-  M5Dial.Display.drawString("Wifi main",
-                            M5Dial.Display.width() / 2,
-                            M5Dial.Display.height() / 2);
+
   delay(1000);
-  while (WiFi.status() != WL_CONNECTED && numRetries > 0)
-  {
-    WiFi.begin(mainWifiSSID, mainWifiPass);
-    delay(1000);
-    numRetries--;
-  }
-  numRetries = 5;
-  if (WiFi.status() != WL_CONNECTED)
-  {
-    M5Dial.Display.clear();
-    M5Dial.Display.drawString("Wifi fallback",
-                              M5Dial.Display.width() / 2,
-                              M5Dial.Display.height() / 2);
-    delay(1000);
-    while (WiFi.status() != WL_CONNECTED && numRetries > 0)
-    {
-      WiFi.begin(fallbackWifiSSID, fallbackWifiPass);
-      delay(1000);
-      numRetries--;
-    }
-  }
+  wifiConnect();
 
   toggl.setAuth(Token);
-  if (WiFi.status() == WL_CONNECTED)
-  {
-    M5Dial.Display.clear();
-    M5Dial.Display.drawString("Wifi conectada",
-                              M5Dial.Display.width() / 2,
-                              M5Dial.Display.height() / 2);
-  }
-  else
-  {
-    M5Dial.Display.clear();
-    M5Dial.Display.drawString("Wifi no conectada",
-                              M5Dial.Display.width() / 2,
-                              M5Dial.Display.height() / 2);
-  }
-  delay(1000);
 }
 
 long oldPosition = -999;
@@ -108,38 +113,84 @@ void loop()
 
     String currentTime = "No time";
 
+    if ((WiFi.status() != WL_CONNECTED))
+    {
+      wifiConnect();
+    }
+
     if ((WiFi.status() == WL_CONNECTED))
     {
-      Serial.println("---- Getting current entry");
-      toggl.GetCurrentTimeEntry(&newTimeEntry);
-      Serial.println("---- Stopping current entry");
-      String httpCode = "";
-      httpCode = toggl.StopTimeEntry(newTimeEntry);
-      Serial.println(httpCode.c_str());
-      Serial.println("Stopped time entry ID:");
-      Serial.println(newTimeEntry.getId());
-      currentTime = formatISODate(toggl.getCurrentTime("UTC"));
-      Serial.println(currentTime.c_str());
-      M5Dial.Display.clear();
-      M5Dial.Speaker.tone(6000, 20);
-      M5Dial.Display.drawString(currentTime.c_str(),
-                                M5Dial.Display.width() / 2,
-                                M5Dial.Display.height() / 2);
-      Serial.println("---- Creating a new entry");
+      int index = ((newPosition % numOfTasks) + numOfTasks) % numOfTasks;
+      if (index == 0)
+      {
+        M5Dial.Display.clear();
+        M5Dial.Display.drawString("Stopping",
+                                  M5Dial.Display.width() / 2,
+                                  M5Dial.Display.height() / 2);
+        Serial.println("---- Getting current entry");
+        toggl.GetCurrentTimeEntry(&newTimeEntry);
+        if (newTimeEntry.getId() == 0)
+        {
+          M5Dial.Display.clear();
+          M5Dial.Display.drawString("Nothing to stop",
+                                    M5Dial.Display.width() / 2,
+                                    M5Dial.Display.height() / 2);
+        }
+        else
+        {
+          Serial.println("---- Stopping current entry");
+          String httpCode = "";
+          httpCode = toggl.StopTimeEntry(newTimeEntry);
+          Serial.println(httpCode.c_str());
+          Serial.println("Stopped time entry ID:");
+          Serial.println(newTimeEntry.getId());
+          M5Dial.Display.clear();
+          std::string msg = "Stopped\r\n" + newTimeEntry.getDescription();
+          M5Dial.Display.drawString(msg.c_str(),
+                                    M5Dial.Display.width() / 2,
+                                    M5Dial.Display.height() / 2);
+          M5Dial.Encoder.readAndReset();
+        }
+      }
+      else
+      {
+        String now = toggl.getCurrentTime("UTC");
+        if (now.length() > 0)
+        {
+          currentTime = formatISODate(now.c_str());
+          Serial.println(currentTime.c_str());
 
-      String tags = "";
-      String timeID = toggl.CreateTimeEntry(favouriteTasks[((newPosition % numOfTasks) + numOfTasks) % numOfTasks].getDescription().c_str(), tags, -1, currentTime.c_str(), 0, "TheToggler_dial", workspaceID, &newTimeEntry);
-      Serial.println(timeID.c_str());
-      Serial.println("New time entry ID:");
-      Serial.println(newTimeEntry.getId());
+          M5Dial.Display.clear();
+          M5Dial.Speaker.tone(6000, 20);
+          M5Dial.Display.drawString(currentTime.c_str(),
+                                    M5Dial.Display.width() / 2,
+                                    M5Dial.Display.height() / 2);
+          Serial.println("---- Creating a new entry");
+
+          String tags = "";
+          String timeID = toggl.CreateTimeEntry(favouriteTasks[index].getDescription().c_str(), tags, -1, currentTime.c_str(), favouriteTasks[index].getProjectId(), "TheToggler_dial", workspaceID, &newTimeEntry);
+          Serial.println(timeID.c_str());
+          Serial.println("New time entry ID:");
+          Serial.println(newTimeEntry.getId());
+          M5Dial.Display.clear();
+          M5Dial.Speaker.tone(6000, 20);
+          M5Dial.Display.drawString(newTimeEntry.getDescription().c_str(),
+                                    M5Dial.Display.width() / 2,
+                                    M5Dial.Display.height() / 2);
+        }
+        else
+        {
+          M5Dial.Display.clear();
+          M5Dial.Display.drawString("No time",
+                                    M5Dial.Display.width() / 2,
+                                    M5Dial.Display.height() / 2);
+        }
+      }
     }
   }
-  if (M5Dial.BtnA.pressedFor(5000))
+  if (M5Dial.BtnA.pressedFor(2000))
   {
-    M5Dial.Encoder.readAndReset();
+
     // M5Dial.Encoder.write(100);
-    M5Dial.Display.drawString("Trackeando",
-                              M5Dial.Display.width() / 2,
-                              M5Dial.Display.height() / 2);
   }
 }
