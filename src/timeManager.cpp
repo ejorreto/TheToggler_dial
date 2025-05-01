9378531536bc45578c09517aff6405b983294959
Enrique Jorreto <ejorreto@gmail.com>
2025-05-01 08:55:33 +0200

    [TTOG-16] Code cleanup

--- src/timeManager.cpp
+++ src/timeManager.cpp
@@ -3,20 +3,11 @@
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 
-#define NTP_TIMEZONE "UTC"
 #define NTP_SERVER1 "0.pool.ntp.org"
 #define NTP_SERVER2 "1.pool.ntp.org"
 #define NTP_SERVER3 "2.pool.ntp.org"
 
-// Different versions of the framework have different SNTP header file names and
-// availability.
-#if __has_include(<esp_sntp.h>)
-#include <esp_sntp.h>
-#define SNTP_ENABLED 1
-#elif __has_include(<sntp.h>)
-#include <sntp.h>
-#define SNTP_ENABLED 1
-#endif
+#include <esp_sntp.h> /* For ESP32-S3, other versions might use different headers */
 
 TimeManager::TimeManager()
 {
@@ -24,86 +15,30 @@ TimeManager::TimeManager()
 
 const String TimeManager::getCurrentTime(const String Timezone)
 {
-  static constexpr const char* const wd[7] = {"Sun", "Mon", "Tue", "Wed",
-    "Thr", "Fri", "Sat"};
   configTzTime(Timezone.c_str(), NTP_SERVER1, NTP_SERVER2, NTP_SERVER3);
 
-#if SNTP_ENABLED
   while (sntp_get_sync_status() != SNTP_SYNC_STATUS_COMPLETED)
   {
+    /** @todo Add a protection limit to avoid entering an infinite loop */
     Serial.print('.');
     delay(1000);
   }
-#else
-  delay(1600);
-  struct tm timeInfo;
-  while (!getLocalTime(&timeInfo, 1000))
-  {
-    Serial.print('.');
-  };
-#endif
 
   Serial.println("\r\n NTP Connected.");
   auto t = time(nullptr);
-  auto tm = gmtime(&t);  // for UTC.
-  Serial.printf("ESP32 UTC  :%04d/%02d/%02d (%s)  %02d:%02d:%02d\r\n",
-                tm->tm_year + 1900, tm->tm_mon + 1, tm->tm_mday,
-                wd[tm->tm_wday], tm->tm_hour, tm->tm_min, tm->tm_sec);
-                // (HH:mm:ss.ssssssZ)
+  /** @todo use something besides gmtime, that uses UTC by default and would be better allowing using a different time zone for flexibility */
+  auto tm = gmtime(&t);
+
   String now = formatTmToISO8601(tm);
-  //  String(tm->tm_year + 1900) + "-" +
-  //        String(tm->tm_mon + 1) + "-" + String(tm->tm_mday) + "T" +
-  //        String(tm->tm_hour) + ":" + String(tm->tm_min) + ":" +
-  //        String(tm->tm_sec) + "Z";
-         Serial.println("Current time: " + now);
-        return now;
+  Serial.println("Current time: " + now);
 
-  /** @todo improve error handling */
-  int16_t HTTP_Code{};
-  String Output{};
-  HTTPClient http;
-
-  http.begin("https://timeapi.io/api/time/current/zone?timeZone=" + Timezone, timeapi_io_ca);
-
-  HTTP_Code = http.GET();
-
-  if (HTTP_Code >= 200 && HTTP_Code <= 226)
-  {
-    // StaticJsonDocument<46> filter;
-    // filter["dateTime"] = true;
-
-    const size_t capacity = JSON_OBJECT_SIZE(4);
-    JsonDocument doc;
-
-    deserializeJson(doc, http.getString());
-
-    const String TMP_Str = doc["dateTime"];
-    Output = formatISODate(TMP_Str);
-    Serial.println("Current time: " + Output);
-  }
-  else
-  {
-    /* Error getting the current time from the time API. Do nothing */
-  }
-
-  http.end();
-  return Output;
+  return now;
 }
 
-String TimeManager::formatISODate(const String &dateTime)
+String TimeManager::formatTmToISO8601(const tm *timeinfo)
 {
-  int decimalPos = dateTime.indexOf('.');
-  if (decimalPos != -1)
-  {
-    return dateTime.substring(0, decimalPos) + "Z";
-  }
-  // If no decimal point found, just append Z
-  return dateTime + "Z";
-}
-
-String TimeManager::formatTmToISO8601(const tm* timeinfo) {
   char buffer[25];
-  snprintf(buffer, sizeof(buffer), 
+  snprintf(buffer, sizeof(buffer),
            "%04d-%02d-%02dT%02d:%02d:%02dZ",
            timeinfo->tm_year + 1900,
            timeinfo->tm_mon + 1,
